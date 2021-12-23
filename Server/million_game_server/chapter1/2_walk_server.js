71d3a9303af9a43b6dfb1a6ae02cb3afd60cb77a
qiaogaojian <qiaogaojian@vip.qq.com>
2021-12-24 00:07:05 +0800

    feat: 接收客户端命令并广播

--- Server/million_game_server/chapter1/2_walk_server.js
+++ Server/million_game_server/chapter1/2_walk_server.js
@@ -1,18 +1,53 @@
 var net = require("net");
 
+class Role {
+  constructor() {
+    this.x = 0;
+    this.y = 0;
+  }
+}
+
+var roles = new Map();
+
 var server = net.createServer(function (socket) {
-  console.log("connected, port:" + socket.remotePort);
+  console.log("connected, portid:" + socket.remotePort);
+  // 新连接
+  roles.set(socket, new Role());
 
   // 接收到数据
   socket.on("data", function (data) {
-    console.log("client send:" + data);
-    var ret = "嗯嗯," + data;
-    socket.write(ret);
+    // console.log("portid-" + socket.remotePort + " send:" + data);
+    var ret = "from server," + data;
+    // socket.write(ret);
+
+    var role = roles.get(socket);
+    var cmd = String(data);
+
+    if (cmd == "left") {
+      console.log("left");
+      role.x--;
+    } else if (cmd == "right") {
+      console.log("right");
+      role.x++;
+    } else if (cmd == "up") {
+      console.log("up");
+      role.y++;
+    } else if (cmd == "down") {
+      console.log("down");
+      role.y--;
+    }
+
+    for (let s of roles.keys()) {
+      var id = socket.remotePort;
+      var str = "portid-" + id + " move to " + role.x + " " + role.y + "\n";
+      s.write(str);
+    }
   });
 
   // 断开连接
   socket.on("close", function () {
-    console.log("closed, port:" + socket.remotePort);
+    console.log("closed, portid:" + socket.remotePort);
+    roles.delete(socket);
   });
 });
 
