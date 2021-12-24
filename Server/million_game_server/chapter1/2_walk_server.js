84e949b3fd35f7d9b79aa65fc668de544bcf9ee4
qiaogaojian <qiaogaojian@vip.qq.com>
2021-12-25 02:48:26 +0800

    feat: 在linux上测试服务器

--- Server/million_game_server/chapter1/2_walk_server.js
+++ Server/million_game_server/chapter1/2_walk_server.js
@@ -16,25 +16,17 @@ var server = net.createServer(function (socket) {
 
   // 接收到数据
   socket.on("data", function (data) {
-    // console.log("portid-" + socket.remotePort + " send:" + data);
-    var ret = "from server," + data;
-    // socket.write(ret);
-
     var role = roles.get(socket);
     var cmd = String(data);
 
-    if (cmd == "left") {
-      console.log("left");
-      role.x--;
-    } else if (cmd == "right") {
-      console.log("right");
-      role.x++;
-    } else if (cmd == "up") {
-      console.log("up");
-      role.y++;
-    } else if (cmd == "down") {
-      console.log("down");
-      role.y--;
+    if (cmd == "left\r\n") {
+        role.x--;
+    } else if (cmd == "right\r\n") {
+        role.x++;
+    } else if (cmd == "up\r\n") {
+        role.y++;
+    } else if (cmd == "down\r\n") {
+        role.y--;
     }
 
     for (let s of roles.keys()) {
