fcd2496dcb8000a58ebcd0732b3a2a92805c6cbe
qiaogaojian <qiaogaojian@vip.qq.com>
2022-01-06 17:47:33 +0800

    feat: 战斗管理器

---
+++ Assets/Mega/Script/Manager/Fight/FightManager.cs
@@ -0,0 +1,43 @@
+﻿using EGamePlay;
+using EGamePlay.Combat;
+using ET;
+
+namespace Mega
+{
+    public class FightManager : GameComponent
+    {
+        private bool isFighting = false;
+
+        /// <summary>
+        /// 初始化战斗管理器
+        /// </summary>
+        public void Init()
+        {
+            MasterEntity.Create();
+            // Entity.Create<TimerComponent>();
+            Entity.Create<CombatContext>();
+            isFighting = true;
+        }
+
+        /// <summary>
+        /// 战斗结束回收资源
+        /// </summary>
+        public void End()
+        {
+            isFighting = false;
+            Entity.Destroy(MasterEntity.Instance);
+            MasterEntity.Destroy();
+        }
+
+        private void Update()
+        {
+            if (!isFighting)
+            {
+                return;
+            }
+
+            MasterEntity.Instance.Update();
+            // TimerComponent.Instance.Update();
+        }
+    }
+}
