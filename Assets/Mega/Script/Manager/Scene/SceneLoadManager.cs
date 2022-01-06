abeed7a2d6b487c17b247e1b21c42c667ed02c49
qiaogaojian <qiaogaojian@vip.qq.com>
2022-01-06 17:47:13 +0800

    feat: 暂时去掉加载动画

--- Assets/Mega/Script/Manager/Scene/SceneLoadManager.cs
+++ Assets/Mega/Script/Manager/Scene/SceneLoadManager.cs
@@ -11,19 +11,8 @@ namespace Mega
         private Action    mFinishJump = null;
         public SceneType CurScene => _SceneType;
 
-
         public void Load(SceneType type, Action onLoad = null)
         {
-            // switch (type)
-            // {
-            //     case SceneType.Game:
-            //         UIManager.Instance.Show(WindowId.UILoading).GetScript<UILoading>().Init(100, LoadingBg.Dungeon, 30);
-            //         break;
-            //     default:
-            //         UIManager.Instance.Show(WindowId.UILoading).GetScript<UILoading>().Init(100, LoadingBg.Default, 30);
-            //         break;
-            // }
-
             if (_SceneType == type)
             {
                 return;
@@ -33,70 +22,12 @@ namespace Mega
             StartCoroutine(ToScene(type));
         }
 
-
-        public void StartLoading(SceneType type, float minDuration = 3f, Action onLoad = null)
-        {
-            if (_SceneType == type)
-            {
-                return;
-            }
-
-            StartCoroutine(ProcessScene(type, minDuration, onLoad));
-        }
-
-        private IEnumerator ProcessScene(SceneType type, float minDuration, Action onLoad = null)
-        {
-            var         go      = Framework.UI.Show(ViewID.UILoading, ViewType.Top).UiObject;
-            LoadingView loading = go.GetComponent<LoadingView>();
-
-            yield return new WaitForEndOfFrame();
-
-            var op = Framework.Scene.JumpToScene(SceneType.Fight);
-
-            //allowSceneActivation 为 false op.progress 最大值为0.89999999
-            op.allowSceneActivation = false;
-
-            var minTimer = minDuration;
-
-            while (!op.isDone)
-            {
-                yield return new WaitForEndOfFrame();
-
-                minTimer -= Time.deltaTime;
-
-                if (minTimer <= 0
-                    && op.progress >= 0.899f
-                    && op.allowSceneActivation != true)
-                {
-                    loading.Progress        = 0.9f;
-                    op.allowSceneActivation = true;
-                }
-                else if (op.progress < 0.9
-                         && loading.Progress < op.progress)
-                {
-                    loading.Progress += 0.01f;
-                }
-                else
-                {
-                    loading.Progress = op.progress;
-                }
-            }
-
-            onLoad?.Invoke();
-
-            yield return new WaitForSeconds(0.2f);
-
-            Framework.UI.Destroy(ViewID.UILoading);
-
-            yield return null;
-        }
-
-        private IEnumerator ToScene(SceneType type)
+        IEnumerator ToScene(SceneType type)
         {
             yield return new WaitForEndOfFrame();
             AsyncOperation op = JumpToScene(type);
-            // op.allowSceneActivation = false;
-            // yield return new WaitForEndOfFrame();
+            op.allowSceneActivation = false;
+            yield return new WaitForEndOfFrame();
             op.allowSceneActivation = true;
 
 
@@ -106,12 +37,9 @@ namespace Mega
                 mFinishJump();
                 mFinishJump = null;
             }
-
-            yield return null;
         }
 
-
-        public AsyncOperation JumpToScene(SceneType type)
+        private AsyncOperation JumpToScene(SceneType type)
         {
             try
             {
