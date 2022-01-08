68fd66ff872eeff94bff90c95491a1ac547676ca
qiaogaojian <qiaogaojian@vip.qq.com>
2022-01-08 15:35:36 +0800

    feat: UI类工具位置

--- Assets/Mega/Script/Manager/UI/UIUtils.cs
+++ Assets/Mega/Script/Manager/UI/UIUtils.cs
@@ -2,7 +2,12 @@
 
 namespace Mega
 {
-    public class UIUtils
+    /// <summary>
+    ///声明扩展的方法的类必须是static
+    ///扩展的方法必须声明为static
+    ///扩展方法必须包含关键字this作为第一个参数类型，后面跟上要扩展的类的名字
+    /// </summary>
+    public static class UIUtils
     {
         public static float getWebMarginH(float designRatio)
         {
@@ -34,6 +39,26 @@ namespace Mega
 
             Debuger.LogWarning($"current side margin Vertical is {margin / 2f}", Debuger.ColorType.cyan);
             return margin / 2f;
+        }
+
+        public static void MarginLeft(this RectTransform rt, float left)
+        {
+            rt.offsetMin = new Vector2(left, rt.offsetMin.y);
+        }
+
+        public static void MarginRight(this RectTransform rt, float right)
+        {
+            rt.offsetMax = new Vector2(-right, rt.offsetMax.y);
+        }
+
+        public static void MarginTop(this RectTransform rt, float top)
+        {
+            rt.offsetMax = new Vector2(rt.offsetMax.x, -top);
+        }
+
+        public static void MarginBottom(this RectTransform rt, float bottom)
+        {
+            rt.offsetMin = new Vector2(rt.offsetMin.x, bottom);
         }
     }
 }
