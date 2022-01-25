7fe571f5a6bf6138245d7d059e18ed170dee4e90
qiaogaojian <qiaogaojian@vip.qq.com>
2022-01-25 11:50:22 +0800

    feat: 完成内存池示例

--- Assets/Scripts/UI/Panel/UIPool.cs
+++ Assets/Scripts/UI/Panel/UIPool.cs
@@ -77,9 +77,12 @@ public class UIPool : BaseView
         Framework.UI.HideCurrent();
     }
 
+    #region 一般对象池 (spawn时不设置父级, 默认会生成在场景中, 跳转场景时会自动销毁)
+
     private void OnClickbtnSpawn()
     {
-        SpawnPool pool      = Framework.Pool.GetPool(pool_btnGetPool);
+        SpawnPool pool = Framework.Pool.GetPool(pool_btnGetPool);
+        // 生成在UI节点下时, 也会持久存在
         Transform curEffect = pool.Spawn(effectPrefab.transform, transform.position, transform.rotation, Framework.UI.GetEffectRoot());
 
         pool1Item.Add(curEffect);
@@ -87,13 +90,13 @@ public class UIPool : BaseView
 
     private void OnClickbtnDespawn()
     {
-        if (pool1Item.Count<=0)
+        if (pool1Item.Count <= 0)
         {
             return;
         }
 
         SpawnPool pool = Framework.Pool.GetPool(pool_btnGetPool);
-        pool.Despawn(pool1Item[Random.Range(0, pool1Item.Count)], 1);  // 延迟1秒后回收
+        pool.Despawn(pool1Item[Random.Range(0, pool1Item.Count)], 1); // 延迟1秒后回收
     }
 
     private void OnClickbtnDestroyPool()
@@ -104,33 +107,71 @@ public class UIPool : BaseView
         pool1Item.Clear();
     }
 
+    #endregion
+
+
+    #region 持久对象池 (对象默认生成在Framework/Pool下面,会持续存在)
+
     private void OnClickbtnSpawnPersist()
     {
-        SpawnPool pool      = Framework.Pool.GetPool(pool_btnCreatePersistPool);
+        SpawnPool pool      = Framework.Pool.CreatePersistPool(pool_btnCreatePersistPool);
         Transform curEffect = pool.Spawn(effectPrefab.transform, transform.position, transform.rotation, Framework.UI.GetEffectRoot());
-        Framework.Pool.GetPool(pool_btnGetPool).Despawn(curEffect, 1);
+
+        pool2Item.Add(curEffect);
     }
 
     private void OnClickbtnDespawnPersist()
     {
+        if (pool2Item.Count <= 0)
+        {
+            return;
+        }
+
+        SpawnPool pool = Framework.Pool.GetPool(pool_btnCreatePersistPool);
+        pool.Despawn(pool2Item[Random.Range(0, pool2Item.Count)], 1); // 延迟1秒后回收
     }
 
     private void OnClickbtnDestroyPersistPool()
     {
+        SpawnPool pool = Framework.Pool.GetPool(pool_btnCreatePersistPool);
+        Framework.Pool.DestroyPool(pool);
+
+        pool2Item.Clear();
     }
+
+    #endregion
+
+
+    #region 预制体对象池 首选 (对象默认生成在Framework/Pool下面,会持续存在,而且会自动回收)
 
     private void OnClickbtnSpawnPrefab()
     {
-        Framework.Pool.CreatePrefabPool(pool_btnCreatePersistPool, effectPrefab.transform);
-        Transform curEffect = Framework.Pool.GetPool(pool_btnCreatePersistPool).Spawn(effectPrefab.transform, transform.position, transform.rotation, Framework.UI.GetEffectRoot());
-        Framework.Pool.GetPool(pool_btnGetPool).Despawn(curEffect, 1);
+        SpawnPool pool      = Framework.Pool.CreatePrefabPool(pool_btnCreatePrefabPool, effectPrefab.transform);
+        Transform curEffect = pool.Spawn(effectPrefab.transform, transform.position, transform.rotation, Framework.UI.GetEffectRoot());
+
+        pool3Item.Add(curEffect);
     }
 
     private void OnClickbtnDeSpawnPrefab()
     {
+        if (pool3Item.Count <= 0)
+        {
+            return;
+        }
+
+        SpawnPool pool        = Framework.Pool.GetPool(pool_btnCreatePrefabPool);
+        int       removeIndex = Random.Range(0, pool3Item.Count);
+        pool.Despawn(pool3Item[removeIndex]); // 立即回收
+        pool3Item.RemoveAt(removeIndex);
     }
 
     private void OnClickbtnDestroyPrefabPool()
     {
+        SpawnPool pool = Framework.Pool.GetPool(pool_btnCreatePrefabPool);
+        Framework.Pool.DestroyPool(pool);
+
+        pool3Item.Clear();
     }
+
+    #endregion
 }
