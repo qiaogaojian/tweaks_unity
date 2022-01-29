d83de401e6ab36252a0ca3e955ea62c667fe03a4
qiaogaojian <qiaogaojian@vip.qq.com>
2022-01-29 17:05:33 +0800

    feat: 拆分UI和FSM

---
+++ Assets/Scripts/Test/TestFsm/Enemy1.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using Mega;
+using UnityEngine;
+
+public class Enemy1 : MonoBehaviour
+{
+    private float      speed = 3f;
+    private AIState    state;
+    private Transform  ivPlayer;
+    private Vector2    homePos;
+    private Quaternion orinRot;
+
+    public void SetTarget(Transform player)
+    {
+        this.ivPlayer = player;
+    }
+
+    public AIState GetCurState()
+    {
+        return state;
+    }
+
+    private void Start()
+    {
+        state   = AIState.Idle;
+        orinRot = transform.rotation;
+        Vector2 enemyPosW = transform.position;
+        homePos = Tools.WorldToScreenPoint(Framework.UI.GetUICamera(), enemyPosW);
+    }
+
+    private void Update()
+    {
+        RectTransform ivEnemy    = transform.GetComponent<RectTransform>();
+        Vector2       enemyPosW  = transform.position;
+        Vector2       playerPosW = ivPlayer.transform.position;
+        Vector2       enemyPos   = Tools.WorldToScreenPoint(Framework.UI.GetUICamera(), enemyPosW);
+        Vector3       mousePos   = Tools.WorldToScreenPoint(Framework.UI.GetUICamera(), playerPosW);
+        float         distance   = Vector2.Distance(enemyPos, mousePos);
+
+        Debuger.Log($"State:{state} EnemyPosition:{enemyPos} MousePosition:{mousePos} Distance:{distance} Angle:{GetAngle(enemyPos, mousePos)} HomePos:{homePos}");
+
+        switch (state)
+        {
+            case AIState.Idle:
+                transform.rotation       = orinRot;
+                ivEnemy.anchoredPosition = Vector2.zero;
+                if (distance <= 500)
+                {
+                    state = AIState.Chase;
+                }
+
+                break;
+            case AIState.Chase:
+                ivEnemy.transform.eulerAngles = new Vector3(ivEnemy.rotation.x, ivEnemy.rotation.y, -GetAngle(enemyPos, mousePos));
+
+                if (distance <= 100)
+                {
+                    state = AIState.Attack;
+                }
+                else if (distance > 500)
+                {
+                    state = AIState.Back;
+                }
+
+                ivEnemy.Translate(-1 * Vector3.up * Time.deltaTime * speed);
+                break;
+            case AIState.Attack:
+                if (distance > 500)
+                {
+                    state = AIState.Back;
+                }
+                else if (distance > 100)
+                {
+                    state = AIState.Chase;
+                }
+
+                ivEnemy.DOLocalMove((ivEnemy.up * 30 + ivEnemy.localPosition), 0.2f).Play();
+                break;
+            case AIState.Back:
+                transform.eulerAngles = new Vector3(0, 0, -GetAngle(enemyPos, homePos));
+                if (distance <= 500)
+                {
+                    state = AIState.Chase;
+                }
+
+                transform.Translate(-1 * Vector3.up * Time.deltaTime * speed);
+                float distanceOri = Vector2.Distance(enemyPos, homePos);
+                if (distanceOri < 10)
+                {
+                    state = AIState.Idle;
+                }
+
+                break;
+        }
+    }
+
+    public float GetAngle(Vector2 from, Vector2 to)
+    {
+        Debuger.LogWarning($" CurPos:{from} HomePos:{to}");
+        Vector2 direction = from - to;
+        float   angle     = Mathf.Atan2(direction.x, direction.y);
+        return angle * Mathf.Rad2Deg;
+    }
+}
--- Assets/Scripts/UI/Panel/UIFsm.cs
+++ Assets/Scripts/UI/Panel/UIFsm.cs
@@ -9,7 +9,7 @@ public class UIFsm : BaseView
 {
     private Button btnReturn;
 
-    private RectTransform   ivEnemy;
+    private Enemy1          ivEnemy;
     private RectTransform   ivPlayer;
     private TextMeshProUGUI tvState;
 
@@ -17,9 +17,11 @@ public class UIFsm : BaseView
     {
         btnReturn = transform.Find("btnReturn").GetComponent<Button>();
 
-        ivEnemy  = transform.Find("ivBg/ivEnemy").GetComponent<RectTransform>();
+        ivEnemy  = transform.Find("ivBg/ivEnemy").GetComponent<Enemy1>();
         ivPlayer = transform.Find("ivBg/ivPlayer").GetComponent<RectTransform>();
         tvState  = transform.Find("ivBg/tvState").GetComponent<TextMeshProUGUI>();
+
+        ivEnemy.SetTarget(ivPlayer);
     }
 
     protected override void AddEvent()
@@ -34,106 +36,19 @@ public class UIFsm : BaseView
 
     private void OnClickBtnReturn()
     {
-        Framework.UI.HideCurrent();
-    }
-
-    #region FSM
-
-    public enum AIState
-    {
-        Idle,
-        Chase,
-        Attack,
-        Back
-    }
-
-    private float      speed = 3f;
-    private AIState    state;
-    private Transform  target;
-    private Vector2    homePos;
-    private Quaternion orinRot;
-
-    private void Start()
-    {
-        state   = AIState.Idle;
-        orinRot = ivEnemy.transform.rotation;
-        Vector2 enemyPosW = ivEnemy.transform.position;
-        homePos = Tools.WorldToScreenPoint(Framework.UI.GetUICamera(), enemyPosW);
+        Framework.UI.DestroyCurrent();
     }
 
     private void Update()
     {
-        Vector2 enemyPosW  = ivEnemy.transform.position;
-        Vector2 playerPosW = ivPlayer.transform.position;
-        Vector2 enemyPos   = Tools.WorldToScreenPoint(Framework.UI.GetUICamera(), enemyPosW);
-        Vector3 mousePos   = Tools.WorldToScreenPoint(Framework.UI.GetUICamera(), playerPosW);
-        float   distance   = Vector2.Distance(enemyPos, mousePos);
-
-        Debuger.Log($"State:{state} EnemyPosition:{enemyPos} MousePosition:{mousePos} Distance:{distance} Angle:{GetAngle(enemyPos, mousePos)} HomePos:{homePos}");
-
-        switch (state)
-        {
-            case AIState.Idle:
-                ivEnemy.transform.rotation = orinRot;
-                ivEnemy.anchoredPosition   = Vector2.zero;
-                if (distance <= 500)
-                {
-                    state = AIState.Chase;
-                }
-
-                break;
-            case AIState.Chase:
-                ivEnemy.eulerAngles = new Vector3(ivEnemy.rotation.x, ivEnemy.rotation.y, -GetAngle(enemyPos, mousePos));
-
-                if (distance <= 100)
-                {
-                    state = AIState.Attack;
-                }
-                else if (distance > 500)
-                {
-                    state = AIState.Back;
-                }
-
-                ivEnemy.Translate(-1 * transform.up * Time.deltaTime * speed);
-                break;
-            case AIState.Attack:
-                if (distance > 500)
-                {
-                    state = AIState.Back;
-                }
-                else if (distance > 100)
-                {
-                    state = AIState.Chase;
-                }
-
-                ivEnemy.DOLocalMove((ivEnemy.up * 30 + ivEnemy.localPosition), 0.2f).Play();
-                break;
-            case AIState.Back:
-                ivEnemy.transform.eulerAngles = new Vector3(ivEnemy.rotation.x, ivEnemy.rotation.y, -GetAngle(enemyPos, homePos));
-                if (distance <= 500)
-                {
-                    state = AIState.Chase;
-                }
-
-                ivEnemy.Translate(-1 * transform.up * Time.deltaTime * speed);
-                float distanceOri = Vector2.Distance(enemyPos, homePos);
-                if (distanceOri < 10)
-                {
-                    state = AIState.Idle;
-                }
-
-                break;
-        }
-
-        tvState.text = $"State: {state}";
+        tvState.text = $"State: {ivEnemy.GetCurState()}";
     }
+}
 
-    public float GetAngle(Vector2 from, Vector2 to)
-    {
-        Vector2 direction = from - to;
-        float   angle     = Mathf.Atan2(direction.x, direction.y);
-        return angle * Mathf.Rad2Deg;
-    }
-
-    #endregion
+public enum AIState
+{
+    Idle,
+    Chase,
+    Attack,
+    Back
 }
