060b8656bde4c83a8136bf4c18d378ecb0ae1e0e
qiaogaojian <qiaogaojian@vip.qq.com>
2022-02-10 17:17:02 +0800

    feat: 异步加载

--- Assets/Scripts/UI/Panel/UIAssetBundle.cs
+++ Assets/Scripts/UI/Panel/UIAssetBundle.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using DG.Tweening;
 using Mega;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,6 +10,7 @@ public class UIAssetBundle : BaseView
 {
     private Button btnReturn;
     private Button btnInsantiate;
+    private Button btnInsantiateAsync;
 
     private Transform root;
 
@@ -17,19 +19,22 @@ public class UIAssetBundle : BaseView
         btnReturn = transform.Find("btnReturn").GetComponent<Button>();
         root      = transform.Find("ivBg");
 
-        btnInsantiate = root.Find("btnInsantiate").GetComponent<Button>();
+        btnInsantiate      = root.Find("btnInsantiate").GetComponent<Button>();
+        btnInsantiateAsync = root.Find("btnInsantiateAsync").GetComponent<Button>();
     }
 
     protected override void AddEvent()
     {
         btnReturn.onClick.AddListener(OnClickBtnReturn);
         btnInsantiate.onClick.AddListener(OnClickBtnInsantiate);
+        btnInsantiateAsync.onClick.AddListener(OnClickBtnInsantiateAsync);
     }
 
     protected override void RemoveEvent()
     {
         btnReturn.onClick.RemoveListener(OnClickBtnReturn);
         btnInsantiate.onClick.RemoveListener(OnClickBtnInsantiate);
+        btnInsantiateAsync.onClick.RemoveListener(OnClickBtnInsantiateAsync);
     }
 
     private void OnClickBtnReturn()
@@ -40,6 +45,11 @@ public class UIAssetBundle : BaseView
     private void OnClickBtnInsantiate()
     {
         InsantiateAsset();
+    }
+
+    private void OnClickBtnInsantiateAsync()
+    {
+        StartCoroutine(InsantiateAssetAsync());
     }
 
     private string assetName  = "BundledSpriteObject";
@@ -56,7 +66,27 @@ public class UIAssetBundle : BaseView
         }
 
         GameObject asset = localAssetBundle.LoadAsset<GameObject>(assetName);
-        Instantiate(asset, root);
+        Instantiate(asset, root).transform.DOMoveX(-1,0.1f);
+        localAssetBundle.Unload(false);
+    }
+
+    IEnumerator InsantiateAssetAsync()
+    {
+        AssetBundleCreateRequest asyncBundleRequest = AssetBundle.LoadFromFileAsync(Path.Combine(Application.streamingAssetsPath, bundleName));
+        yield return asyncBundleRequest;
+        AssetBundle localAssetBundle = asyncBundleRequest.assetBundle;
+
+        if (localAssetBundle == null)
+        {
+            Debuger.LogError("Failed to load AssetBundle!");
+            yield break;
+        }
+
+        AssetBundleRequest assetRequest = localAssetBundle.LoadAssetAsync<GameObject>(assetName);
+        yield return assetRequest;
+
+        GameObject prefab = assetRequest.asset as GameObject;
+        Instantiate(prefab, root).transform.DOMoveX(1,0.1f);
         localAssetBundle.Unload(false);
     }
 }
